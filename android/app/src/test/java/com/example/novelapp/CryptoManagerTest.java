9e8841f75fb5f5689881b15aabe98e09c2d3f08e
이성면 <shipistz@gmail.com>
2026-02-08 12:39:27 +0900

    수정

--- android/app/src/test/java/com/example/novelapp/CryptoManagerTest.java
+++ android/app/src/test/java/com/example/novelapp/CryptoManagerTest.java
@@ -8,7 +8,8 @@ public class CryptoManagerTest {
     @Test
     public void testAuthSignatureGeneration() {
         String publicKey = "testPublicKey";
-        kotlin.Pair<Long, String> auth = CryptoManager.INSTANCE.generateAuth(publicKey);
+        String salt = "testSaltBase64"; // Dummy Salt
+        kotlin.Pair<Long, String> auth = CryptoManager.INSTANCE.generateAuth(publicKey, salt);
 
         long timestamp = auth.getFirst();
         String signature = auth.getSecond();
